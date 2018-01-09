c425ccb26e48b278625440b1edc3c89bd835a743
Xceno <Xceno@protonmail.com>
2018-01-09 22:55:47 +0100

    feat: Sitemap support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Xceno.io`
+    title: `Xceno.io`,
+    siteUrl: "https://www.xceno.io"
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -33,6 +34,28 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-netlify`,
-    `gatsby-plugin-remove-trailing-slashes`
+    `gatsby-plugin-remove-trailing-slashes`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/sitemap.xml`,
+        query: `
+          {
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+
+            allSitePage(filter: {path: {glob: "/**?[!scripts]**" }}) {
+              edges {
+                node {
+                  path
+                }
+              }
+            }
+        }`
+      }
+    }
   ]
 };
