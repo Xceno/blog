26a7b52c2350cc0d4a78c732f02544f834fcfbfc
Xceno <Xceno@protonmail.com>
2019-05-25 01:42:29 +0200

    feat: Finally add an RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,6 +52,71 @@ module.exports = {
             }
         }`
       }
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+                language
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  categories: edge.node.frontmatter.tags,
+                  custom_elements: [
+                    {
+                      "content:encoded": edge.node.html
+                    }
+                  ]
+                });
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                        tags
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            feed_url: "https://www.xceno.io/rss.xml",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            match: "^/posts/"
+          }
+        ]
+      }
     }
   ]
 };
